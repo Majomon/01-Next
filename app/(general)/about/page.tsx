8ef832f42f09f3300fbab90d32001977d21ad638
Mauri <mauri.monzon91@gmail.com>
2024-04-04 22:10:29 -0300

    Navegacion

--- app/(general)/about/page.tsx
+++ app/(general)/about/page.tsx
@@ -9,7 +9,6 @@ export const metadata: Metadata = {
 export default function AboutPage() {
   return (
     <main className="flex flex-col items-center p-24">
-      <span className="text-lg">Hola mundo</span>
       <span className="text-7xl">About Page</span>
     </main>
   );
